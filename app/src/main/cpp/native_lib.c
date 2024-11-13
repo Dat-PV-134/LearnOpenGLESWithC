bece2dbaa8ac3f258ab076a8e3aac43d9af2e5a1
Phạm Văn Đạt <94840545+Dat-PV-134@users.noreply.github.com>
2024-11-13 16:57:39 +0700

    update base

--- app/src/main/cpp/native_lib.c
+++ app/src/main/cpp/native_lib.c
@@ -28,13 +28,6 @@ unsigned int VBO, VAO, shaderProgram, vertexShader, fragmentShader;
 void on_surface_created() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-    // Create and bind the vertex array object and buffer
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     // Compile the vertex shader
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -69,17 +62,23 @@ void on_surface_created() {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Shader program linking failed: %s", infoLog);
     }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
+    // Create and bind the vertex array object and buffer
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Define the vertex attribute pointer
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Use the shader program and bind the VAO
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 void on_surface_changed() {
@@ -90,6 +89,8 @@ void on_draw_frame() {
     glClear(GL_COLOR_BUFFER_BIT);  // Clear the screen
 
     // Draw the triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
