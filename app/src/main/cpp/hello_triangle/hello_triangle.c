6708384a5e8fa81c686459c9d35d48183e582ca1
Phạm Văn Đạt <94840545+Dat-PV-134@users.noreply.github.com>
2024-11-14 20:56:59 +0700

    draw color for each vertex

--- app/src/main/cpp/hello_triangle/hello_triangle.c
+++ app/src/main/cpp/hello_triangle/hello_triangle.c
@@ -5,20 +5,22 @@
 #include <android/log.h> // For logging errors
 #include "hello_triangle.h"
 
-const char *vertexShaderSourceZipper = "#version 300 es\n"
-                                 "in vec3 aPos;\n"
+const char *vertexShaderSource = "#version 300 es\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   ourColor = aColor;\n"
                                  "}\0";
 
-const char *fragmentShaderSourceZipper = "#version 300 es\n"
-                                   "precision mediump float;\n"
-                                   "uniform vec3 triangleColor;\n"
+const char *fragmentShaderSource = "#version 300 es\n"
+                                   "in vec3 ourColor;\n"
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = vec4(triangleColor.x, triangleColor.y, triangleColor.z, 1.0f);\n"
+                                   "    FragColor = vec4(ourColor.x, ourColor.y, ourColor.z, 1.0f);\n"
                                    "}\0";
 
 //float vertices[] = {
@@ -28,17 +30,17 @@ const char *fragmentShaderSourceZipper = "#version 300 es\n"
 //};
 
 float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
+        0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom left
+        -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 1.0f, // top left
 };
 unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
 };
 
-unsigned int VBOZipper, VAOZipper, EBOZipper, shaderProgramZipper, vertexShaderZipper, fragmentShaderZipper;
+unsigned int VBO, VAO, EBO, shaderProgram, vertexShader, fragmentShader;
 
 clock_t t_start;
 
@@ -46,56 +48,58 @@ void on_surface_created() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     // Compile the vertex shader
-    vertexShaderZipper = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShaderZipper, 1, &vertexShaderSourceZipper, NULL);
-    glCompileShader(vertexShaderZipper);
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
     int success;
-    glGetShaderiv(vertexShaderZipper, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetShaderInfoLog(vertexShaderZipper, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Vertex shader compilation failed: %s", infoLog);
     }
 
     // Compile the fragment shader
-    fragmentShaderZipper = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShaderZipper, 1, &fragmentShaderSourceZipper, NULL);
-    glCompileShader(fragmentShaderZipper);
-    glGetShaderiv(fragmentShaderZipper, GL_COMPILE_STATUS, &success);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetShaderInfoLog(fragmentShaderZipper, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Fragment shader compilation failed: %s", infoLog);
     }
 
     // Link shaders to the program
-    shaderProgramZipper = glCreateProgram();
-    glAttachShader(shaderProgramZipper, vertexShaderZipper);
-    glAttachShader(shaderProgramZipper, fragmentShaderZipper);
-    glLinkProgram(shaderProgramZipper);
-    glGetProgramiv(shaderProgramZipper, GL_LINK_STATUS, &success);
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetProgramInfoLog(shaderProgramZipper, 512, NULL, infoLog);
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Shader program linking failed: %s", infoLog);
     }
-    glDeleteShader(vertexShaderZipper);
-    glDeleteShader(fragmentShaderZipper);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // Create and bind the vertex array object and buffer
-    glGenVertexArrays(1, &VAOZipper);
-    glGenBuffers(1, &VBOZipper);
-    glGenBuffers(1, &EBOZipper);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAOZipper);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOZipper);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOZipper);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Define the vertex attribute pointer
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -109,14 +113,8 @@ void on_draw_frame() {
     glClear(GL_COLOR_BUFFER_BIT);  // Clear the screen
 
     // Draw the triangle
-    glUseProgram(shaderProgramZipper);
-    glBindVertexArray(VAOZipper);
-
-    GLint uniColor = glGetUniformLocation(shaderProgramZipper, "triangleColor");
-    clock_t t_now = clock();  // Get the current time
-    float time = ((float)(t_now - t_start)) / 100000;  // Convert clock ticks to seconds
-    // Use the elapsed time to calculate the color (sinusoidal wave for red)
-    glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
--- app/src/main/cpp/zipper_lock/zipper_lock.c
+++ app/src/main/cpp/zipper_lock/zipper_lock.c
@@ -5,14 +5,14 @@
 #include <android/log.h> // For logging errors
 #include "zipper_lock.h"
 
-const char *vertexShaderSource = "#version 300 es\n"
+const char *vertexShaderSourceZipper = "#version 300 es\n"
                                  "in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
 
-const char *fragmentShaderSource = "#version 300 es\n"
+const char *fragmentShaderSourceZipper = "#version 300 es\n"
                                    "precision mediump float;\n"
                                    "uniform vec3 triangleColor;\n"
                                    "out vec4 FragColor;\n"
@@ -38,57 +38,57 @@ unsigned int indicesZipper[] = {  // note that we start from 0!
         1, 2, 3    // second triangle
 };
 
-unsigned int VBO, VAO, EBO, shaderProgram, vertexShader, fragmentShader;
+unsigned int VBOZipper, VAOZipper, EBOZipper, shaderProgramZipper, vertexShaderZipper, fragmentShaderZipper;
 
 void on_surface_created_zipper() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     // Compile the vertex shader
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
+    vertexShaderZipper = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShaderZipper, 1, &vertexShaderSourceZipper, NULL);
+    glCompileShader(vertexShaderZipper);
     int success;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(vertexShaderZipper, GL_COMPILE_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShaderZipper, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Vertex shader compilation failed: %s", infoLog);
     }
 
     // Compile the fragment shader
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    fragmentShaderZipper = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderZipper, 1, &fragmentShaderSourceZipper, NULL);
+    glCompileShader(fragmentShaderZipper);
+    glGetShaderiv(fragmentShaderZipper, GL_COMPILE_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShaderZipper, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Fragment shader compilation failed: %s", infoLog);
     }
 
     // Link shaders to the program
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    shaderProgramZipper = glCreateProgram();
+    glAttachShader(shaderProgramZipper, vertexShaderZipper);
+    glAttachShader(shaderProgramZipper, fragmentShaderZipper);
+    glLinkProgram(shaderProgramZipper);
+    glGetProgramiv(shaderProgramZipper, GL_LINK_STATUS, &success);
     if (!success) {
         char infoLog[512];
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        glGetProgramInfoLog(shaderProgramZipper, 512, NULL, infoLog);
         __android_log_print(ANDROID_LOG_ERROR, "OpenGL", "Shader program linking failed: %s", infoLog);
     }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShaderZipper);
+    glDeleteShader(fragmentShaderZipper);
 
     // Create and bind the vertex array object and buffer
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAOZipper);
+    glGenBuffers(1, &VBOZipper);
+    glGenBuffers(1, &EBOZipper);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(VAOZipper);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOZipper);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verticesZipper), verticesZipper, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOZipper);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesZipper), indicesZipper, GL_STATIC_DRAW);
 
     // Define the vertex attribute pointer
@@ -107,10 +107,10 @@ void on_draw_frame_zipper() {
     glClear(GL_COLOR_BUFFER_BIT);  // Clear the screen
 
     // Draw the triangle
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
+    glUseProgram(shaderProgramZipper);
+    glBindVertexArray(VAOZipper);
 
-    GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+    GLint uniColor = glGetUniformLocation(shaderProgramZipper, "triangleColor");
     glUniform3f(uniColor, 1.0f, 0.5f, 0.2f);
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
